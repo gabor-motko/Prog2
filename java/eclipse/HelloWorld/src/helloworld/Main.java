0f64178bad9261d94627b80e794a90e28a62f3ec
gabor-motko <gabor.motko.95@gmail.com>
2018-03-07 17:36:06 +0100

    uhhhhhhh

--- java/eclipse/HelloWorld/src/helloworld/Main.java
+++ java/eclipse/HelloWorld/src/helloworld/Main.java
@@ -2,7 +2,6 @@
  * 
  */
 package helloworld;
-import java.util.*;
 /**
  * @author hallgato
  *
@@ -15,20 +14,30 @@ public class Main
 	 */
 	public static void main(String[] args)
 	{
-		Person p1 = new Person("Gipsz Jakab", 38, true);
-		Person p2 = new Person();
-		Person p3 = new Person("Béla", 15, true);
+//		Person p1 = new Person("Gipsz Jakab", 38, true);
+//		Person p2 = new Person();
+//		Person p3 = new Person("Béla", 15, true);
+//		
+//		p2.setName("Apophis");
+//		p2.setAge(5000);
+//		
+//		System.out.println(p1);
+//		System.out.println(p2);
+//		System.out.println(p3);
+//		
+//		Scanner s = new Scanner(System.in);
+//		s.useLocale(new Locale("en"));
+//		System.out.println(s.nextInt());
+//		s.close();
+		Hungarian h = new Hungarian();
+		English e = new English();
+		Communication t[] = new Communication[2];
+		t[0] = h;
+		t[1] = e;
 		
-		p2.setName("Apophis");
-		p2.setAge(5000);
-		
-		System.out.println(p1);
-		System.out.println(p2);
-		System.out.println(p3);
-		
-		Scanner s = new Scanner(System.in);
-		s.useLocale(new Locale("en"));
-		System.out.println(s.nextInt());
-		s.close();
+		for(Communication p : t)
+		{
+		    p.hello("fucker");
+		}
 	}
 }
