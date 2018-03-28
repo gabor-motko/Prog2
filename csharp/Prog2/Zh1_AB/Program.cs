3fff17dd22168515a9a974ba9e1faa1545d0faf0
hallgato <hallgato@local>
2018-03-28 16:25:19 +0200

    zh feladatok

---
+++ csharp/Prog2/Zh1_AB/Program.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zh1_B
+{
+    class RC : IComparable<RC>
+    {
+        public RC(string name, string location, int heightLimit, int waitingTime)
+        {
+            Name = name;
+            Location = location;
+            HeightLimit = heightLimit;
+            WaitingTime = waitingTime;
+        }
+
+        public string Name { get; set; }
+        public string Location { get; set; }
+        public int HeightLimit { get; set; }
+        public int WaitingTime { get; set; }
+
+        public int CompareTo(RC other)
+        {
+            if (this.WaitingTime != other.WaitingTime)
+                return this.WaitingTime.CompareTo(other.WaitingTime);
+            if (this.HeightLimit != other.HeightLimit)
+                return -this.HeightLimit.CompareTo(other.HeightLimit);
+            return this.Name.CompareTo(other.Name);
+        }
+
+        public override string ToString()
+        {
+            return this.Name + " (" + this.Location + "): " + this.WaitingTime;
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            RC[] t = new RC[n];
+
+            for (int i = 0; i < n; ++i)
+            {
+                string[] line = Console.ReadLine().Split(';');
+                t[i] = new RC(line[0], line[1], int.Parse(line[2]), int.Parse(line[3]));
+            }
+
+            Array.Sort(t);
+
+            foreach (RC e in t)
+                Console.WriteLine(e);
+        }
+    }
+}
---
+++ csharp/Prog2/Zh1_CD/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Zh1_D
+{
+    class Program
+    {
+        enum Operator { None, And, Or, Implication, Equivalence }
+        static void Main(string[] args)
+        {
+            string line;
+            Operator op = Operator.None;
+            while ((line = Console.ReadLine()) != null)
+            {
+                string[] s = line.Split(' ');
+                if (line[0] == '1' || line[0] == '0')    //interpretáció
+                {
+                    bool a = s[0] == "1";
+                    bool b = s[1] == "1";
+                    switch (op)
+                    {
+                        case Operator.None:
+                            break;
+                        case Operator.And:
+                            Console.WriteLine(a && b ? '1' : '0');
+                            break;
+                        case Operator.Or:
+                            Console.WriteLine(a || b ? '1' : '0');
+                            break;
+                        case Operator.Implication:
+                            Console.WriteLine((a == true && b == false) ? '0' : '1');
+                            break;
+                        case Operator.Equivalence:
+                            Console.WriteLine(a == b ? '1' : '0');
+                            break;
+                        default:
+                            break;
+                    }
+                }
+                else
+                {
+                    //formula
+                    Console.WriteLine(line);
+                    switch (s[1])
+                    {
+                        case "=":
+                            op = Operator.Equivalence;
+                            break;
+                        case ">":
+                            op = Operator.Implication;
+                            break;
+                        case "&":
+                            op = Operator.And;
+                            break;
+                        case "|":
+                            op = Operator.Or;
+                            break;
+                        default:
+                            op = Operator.None;
+                            break;
+                    }
+                }
+            }
+        }
+    }
+}
