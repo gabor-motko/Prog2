7aba784e04650b1ed0d1eb775776d57812e6a3f2
hallgato <hallgato@local>
2018-04-18 17:30:12 +0200

    c# work

---
+++ csharp/Prog2/Files/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace Files
+{
+    class Program
+    {
+        static string path = "file.txt";
+        static void Main(string[] args)
+        {
+            List<string> lines = new List<string>();
+            try
+            {
+                using (StreamReader r = new StreamReader(path))
+                {
+                    string line;
+                    while((line = r.ReadLine()) != null)
+                    {
+                        lines.Add(line);
+                    }
+                }
+            }
+            catch(Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+            List<string>.Enumerator e = lines.GetEnumerator();
+            while(e.MoveNext())
+            {
+                Console.WriteLine(e.Current);
+            }
+            //foreach(var l in lines)
+            //{
+            //    Console.WriteLine(l);
+            //}
+        }
+    }
+}
