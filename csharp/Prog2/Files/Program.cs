2d1f5b21bfe1fc465ad04cc08e31d27c5e56d95a
hallgato <hallgato@local>
2018-04-18 17:40:56 +0200

    c# work

--- csharp/Prog2/Files/Program.cs
+++ csharp/Prog2/Files/Program.cs
@@ -9,27 +9,31 @@ namespace Files
 {
     class Program
     {
-        static string path = "file.txt";
+        static readonly string path = "file.txt";
+        static readonly string pathOut = "out.txt";
         static void Main(string[] args)
         {
             List<string> lines = new List<string>();
             try
             {
+                using (StreamWriter w = new StreamWriter(pathOut, true))
                 using (StreamReader r = new StreamReader(path))
                 {
                     string line;
-                    while((line = r.ReadLine()) != null)
+                    while ((line = r.ReadLine()) != null)
                     {
                         lines.Add(line);
+                        w.WriteLine(line.ToUpper());
                     }
+                    w.WriteLine();
                 }
             }
-            catch(Exception ex)
+            catch (Exception ex)
             {
                 Console.WriteLine(ex.ToString());
             }
             List<string>.Enumerator e = lines.GetEnumerator();
-            while(e.MoveNext())
+            while (e.MoveNext())
             {
                 Console.WriteLine(e.Current);
             }
