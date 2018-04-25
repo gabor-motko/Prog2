b62d5045be7e41a1d473c4e54b008f3b4d201531
hallgato <hallgato@local>
2018-04-25 17:01:52 +0200

    wrote a shell

---
+++ csharp/Prog2/Shell/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using ShellDLL;
+
+namespace ShellTest
+{
+    class CalculatorShell : Shell
+    {
+
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Shell sh = new CalculatorShell();
+            sh.ReadEvalPrint();
+        }
+    }
+}
