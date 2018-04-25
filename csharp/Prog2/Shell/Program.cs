440e615dfd626eb20689834442d8f898e6c41dae
hallgato <hallgato@local>
2018-04-25 17:41:43 +0200

    finished shell

--- csharp/Prog2/Shell/Program.cs
+++ csharp/Prog2/Shell/Program.cs
@@ -8,9 +8,81 @@ using ShellDLL;
 
 namespace ShellTest
 {
+    class ExitCommand : Command
+    {
+        private Shell sender;
+        public ExitCommand(Shell sender) : base("exit")
+        {
+            this.sender = (CalculatorShell)sender;
+        }
+
+        public override bool Execute(params string[] args)
+        {
+            sender.Exit(0);
+            return true;
+        }
+
+    }
+
+    class PlusCommand : Command
+    {
+        private CalculatorShell sender;
+        public PlusCommand(Shell sender) : base("+")
+        {
+            this.sender = (CalculatorShell)sender;
+        }
+
+        public override bool Execute(params string[] args)
+        {
+            float val;
+            if (args.Length <= 0 || !float.TryParse(args[0], out val))
+            {
+                return false;
+            }
+            sender.Value += val;
+            sender.Format(sender.ToString());
+            return true;
+        }
+    }
+
+    class MinusCommand : Command
+    {
+        private CalculatorShell sender;
+        public MinusCommand(Shell sender) : base("-")
+        {
+            this.sender = (CalculatorShell)sender;
+        }
+
+        public override bool Execute(params string[] args)
+        {
+            float val;
+            if (args.Length <= 0 || !float.TryParse(args[0], out val))
+            {
+                return false;
+            }
+            sender.Value -= val;
+            sender.Format(sender.ToString());
+            return true;
+        }
+    }
+
     class CalculatorShell : Shell
     {
+        public float Value { get; set; }
+        public override string ToString()
+        {
+            return string.Format($"{this.Value:F2}");
+        }
 
+        protected override void Init()
+        {
+            base.Init();
+            this.Value = 0.0f;
+            this.AddCommand(new ExitCommand(this));
+            this.AddCommand(new PlusCommand(this));
+            this.AddCommand(new MinusCommand(this));
+            Format(this.ToString());
+        }
     }
 
     class Program
